8c97cc713126462cddbaa03f0a21823a811bccb9
aayman01 <ayman.alfi098@gmail.com>
2024-03-16 00:49:24 +0600

    nav bar done

--- src/Components/Nav_bar/Navbar.jsx
+++ src/Components/Nav_bar/Navbar.jsx
@@ -1,10 +1,75 @@
+import proflie_img from '../../assets/images/Frame.jpg'
 
 const Navbar = () => {
-    return (
-        <div>
-            <h3 className="font-bold">Hi!!!</h3>
+  return (
+    <div className='container mx-auto'>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+          </div>
+          <a className="btn btn-ghost text-3xl text-[#150B2B] font-bold">
+            Recipe Calories
+          </a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-[#150B2BB3] text-base">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Recipes</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Search</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <label className="input input-bordered rounded-3xl flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <input type="text" className="grow" placeholder="Search" />
+          </label>
+          <button className="ml-4 mr-4">
+            <img
+              className="rounded-full bg-[#0BE58A]"
+              src={proflie_img}
+              alt=""
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
